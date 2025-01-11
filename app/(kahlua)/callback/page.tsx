08bcf62c4fde95a5dc8e2f1f063a238bafa34ab9
limgahyun <limgh2002@gmail.com>
2025-01-11 16:12:44 +0900

    fix: login api url 수정

--- app/(kahlua)/callback/page.tsx
+++ app/(kahlua)/callback/page.tsx
@@ -20,11 +20,12 @@ const page = () => {
   useEffect(() => {
     const query = new URLSearchParams(window.location.search);
     const code = query.get('code');
+    const redirectUri = `${process.env.NEXT_PUBLIC_KAKAO_LOCAL_REDIRECT_URI}`;
 
     const handleAuth = async () => {
       try {
         const response = await axiosInstance.post(
-          `/auth/kakao/sign-in?code=${code}`
+          `/auth/kakao/sign-in?code=${code}&redirectUri=${redirectUri}`
         );
 
         const { accessToken, refreshToken } = response.data;
